ac89a79559df8721aee81c38a875d27914c79267
Chip Shabazian <chip@shabazian.com>
2024-10-03 16:40:18 -0700

    Added mkdir if build doesn't exist

--- make_rpm8.sh
+++ make_rpm8.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+[[ -d build ]] || mkdir build
 rm -rf ./build/*
 podman run --rm -it -v $(pwd)/build:/efs-utils/build:Z localhost/rhel8/efs-utils:latest /root/build_efs.sh
--- make_rpm9.sh
+++ make_rpm9.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+[[ -d build ]] || mkdir build
 rm -rf ./build/*
 podman run --rm -it -v $(pwd)/build:/efs-utils/build:Z localhost/rhel9/efs-utils:latest /root/build_efs.sh
