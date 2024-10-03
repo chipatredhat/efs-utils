0b6f4db7d06e80f7bce1331cb4c3506af5b90493
Chip Shabazian <chip@shabazian.com>
2024-10-03 16:44:41 -0700

    Updated make scripts to use different directories for RHEL 8 and 9

--- make_rpm9.sh
+++ make_rpm9.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-[[ -d build ]] || mkdir build
-rm -rf ./build/*
-podman run --rm -it -v $(pwd)/build:/efs-utils/build:Z localhost/rhel9/efs-utils:latest /root/build_efs.sh
+[[ -d rhel9/build ]] || mkdir rhel9/build
+rm -rf ./rhel9/build/*
+podman run --rm -it -v $(pwd)/rhel9/build:/efs-utils/build:Z localhost/rhel9/efs-utils:latest /root/build_efs.sh
