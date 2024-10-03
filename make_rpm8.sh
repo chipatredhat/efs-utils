a754cd880379953aa747fdac67702e2f40d690a7
Chip Shabazian <chip@shabazian.com>
2024-10-03 16:48:01 -0700

    Fixed make_rpm

--- make_rpm8.sh
+++ make_rpm8.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-[[ -d rhel8/build ]] || mkdir rhel8/build
+[[ -d rhel8/build ]] || mkdir -p rhel8/build
 rm -rf ./rhel8/build/*
 podman run --rm -it -v $(pwd)/rhel8/build:/efs-utils/build:Z localhost/rhel8/efs-utils:latest /root/build_efs.sh
--- make_rpm9.sh
+++ make_rpm9.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-[[ -d rhel9/build ]] || mkdir rhel9/build
+[[ -d rhel9/build ]] || mkdir -p rhel9/build
 rm -rf ./rhel9/build/*
 podman run --rm -it -v $(pwd)/rhel9/build:/efs-utils/build:Z localhost/rhel9/efs-utils:latest /root/build_efs.sh
